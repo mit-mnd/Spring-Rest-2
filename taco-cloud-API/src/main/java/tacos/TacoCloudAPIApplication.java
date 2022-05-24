95b719fe180873068fe47af86447a2eb6ce51be8
mit-mnd <99387780+mit-mnd@users.noreply.github.com>
2022-05-24 07:44:53 +0700

    Add files via upload

---
+++ taco-cloud-API/src/main/java/tacos/TacoCloudAPIApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudAPIApplication.class, args);
+	}
+
+}
---
+++ taco-cloud-API/src/main/java/tacos/data/IngredientRepository.java
@@ -0,0 +1,12 @@
+package tacos.data;
+
+import tacos.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IngredientRepository extends JpaRepository<Ingredient, String> {
+    Optional<Ingredient> findById(String id);
+}
